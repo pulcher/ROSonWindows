e0bb2c98e0631b99ab03b8d7ea9ee721897fac7c
Harold Pulcher <pulcher@killercomputing.com>
2020-04-26 00:22:04 -0500

    starting to work on the c# console demo application

---
+++ Examples/roboClaw-console/roboClaw-console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace roboClaw_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var devices = RoboclawClassLib.Roboclaw.GetDevices();
+
+            Console.WriteLine("found devices:");
+
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"{device.Description}");
+            }
+        }
+    }
+}
