f1442b60b4f82dee633c452f367db6007f6c5219
Harold Pulcher <pulcher@killercomputing.com>
2020-09-25 21:39:06 -0500

    got the remote copy in place, although it needs some more work

---
+++ Examples/CSharp/ConsoleFramework/ConsoleFramework/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Lattepanda! - again ");
+
+            Console.WriteLine("press any key to end....");
+            Console.ReadKey();
+        }
+    }
+}
