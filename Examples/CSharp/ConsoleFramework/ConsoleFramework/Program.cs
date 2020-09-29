8a4640635dc4fc112e93b160e83e8b2093fbf821
Harold Pulcher <pulcher@killercomputing.com>
2020-09-28 21:21:28 -0500

    adding console apps to examples for controlling stuff through firmata

--- Examples/CSharp/ConsoleFramework/ConsoleFramework/Program.cs
+++ Examples/CSharp/ConsoleFramework/ConsoleFramework/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using LattePanda.Firmata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleFramework
@@ -12,8 +14,18 @@ namespace ConsoleFramework
         {
             Console.WriteLine("Hello, Lattepanda! - again ");
 
-            Console.WriteLine("press any key to end....");
-            Console.ReadKey();
+            Arduino arduino = new Arduino("COM7", 250000, true, 8000);
+            arduino.pinMode(13, Arduino.OUTPUT);//Set the digital pin 13 as output
+            while (true)
+            {
+                Console.Write("on...");
+                // ==== set the led on or off
+                arduino.digitalWrite(13, Arduino.HIGH);//set the LED　on
+                Thread.Sleep(10);//delay a seconds
+                Console.WriteLine(" off");
+                arduino.digitalWrite(13, Arduino.LOW);//set the LED　off
+                Thread.Sleep(10);//delay a seconds
+            }
         }
     }
 }
