0ef76867b9263151a8449388c745721f86462232
Harold Pulcher <pulcher@killercomputing.com>
2020-10-12 22:04:00 -0500

    got some numbers from the joystick

--- Examples/CSharp/gamepadUWP/gamepadUWP/MainPage.xaml.cs
+++ Examples/CSharp/gamepadUWP/gamepadUWP/MainPage.xaml.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Timers;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Gaming.Input;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -19,8 +22,13 @@ namespace gamepadUWP
     public sealed partial class MainPage : Page
     {
         private readonly object myLock = new object();
+        private readonly Brush DefaultButtonBackground = new SolidColorBrush(Colors.AntiqueWhite);
+        private readonly Brush HighlightedButtonBackground = new SolidColorBrush(Colors.Orange);
         private List<RawGameController> myGamepads = new List<RawGameController>();
         private RawGameController mainGamepad;
+        private bool[] buttonArray;
+        private GameControllerSwitchPosition[] switchArray;
+        private double[] axisArray;
 
         public MainPage()
         {
@@ -29,12 +37,17 @@ namespace gamepadUWP
             GameControllersSetup();
 
             GetRawGameControllers();
+
+            var pollingTimer = new Timer();
+            pollingTimer.Elapsed += new ElapsedEventHandler(PollTimeEvent);
+            pollingTimer.Interval = 100;
+            pollingTimer.Start();
         }
 
-        private void GameControllersSetup()
+        private async Task GameControllersSetup()
         {
             // setup delegates for adding/removing to the gamepad list
-            RawGameController.RawGameControllerAdded += (object sender, RawGameController e) =>
+            RawGameController.RawGameControllerAdded += async (object sender, RawGameController e) =>
             {
                 //Check if the just - added gamepad is already in myGamepads; if it isn't, add
                 // it.
@@ -43,7 +56,21 @@ namespace gamepadUWP
 
                 if (!gamepadInList)
                 {
+                    buttonArray = new bool[e.ButtonCount];
+                    switchArray = new GameControllerSwitchPosition[e.SwitchCount];
+                    axisArray = new double[e.AxisCount];
+
                     myGamepads.Add(e);
+                }
+
+                // add a placeholder for the number of buttons
+                for (int i = 0; i < e.ButtonCount; i++)
+                {
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        ButtonPanelHolder.Children.Add(CreateButtonDisplay(i));
+                    });
+
                 }
             };
 
@@ -66,6 +93,21 @@ namespace gamepadUWP
             };
         }
 
+        private UIElement CreateButtonDisplay(int buttonNumber)
+        {
+            var border = new Border();
+            border.Background = DefaultButtonBackground;
+            border.Child = new TextBlock
+            {
+                Text = buttonNumber.ToString("D2"),
+                Foreground = new SolidColorBrush(Colors.Black),
+                FontSize = 12,
+                Margin = new Thickness(0, 0, 5, 0)
+            };
+
+            return border;
+        }
+
         private void GetRawGameControllers()
         {
             foreach (var controller in myGamepads)
@@ -75,6 +117,49 @@ namespace gamepadUWP
 
                 var ugh = string.Empty;
             }
+        }
+
+        private void PollTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            if (myGamepads.Count > 0)
+                PollRawGameControllers();
+        }
+
+        private void PollRawGameControllers()
+        {
+            var gamepad = myGamepads[0];
+
+            var reading = gamepad.GetCurrentReading(buttonArray, switchArray, axisArray);
+
+            UpdateButtonStatus();
+        }
+
+        private async void UpdateButtonStatus()
+        {
+
+            var buttonsDisplayed = ButtonPanelHolder.Children;
+            for (int i = 0; i < buttonArray.Count(); i++)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    var currentBorder = buttonsDisplayed[i] as Border;
+                    if (buttonArray[i])
+                        currentBorder.Background = HighlightedButtonBackground;
+                    else
+                        currentBorder.Background = DefaultButtonBackground;
+
+                });
+            };
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                LeftXValue.Text = axisArray[0].ToString("F6");
+                LeftYValue.Text = axisArray[1].ToString("F6");
+                RightXValue.Text = axisArray[2].ToString("F6");
+                RightYValue.Text = axisArray[3].ToString("F6");
+                DPadXValue.Text = axisArray[4].ToString("F6");
+                DPadYValue.Text = axisArray[5].ToString("F6");
+            });
         }
     }
 }
